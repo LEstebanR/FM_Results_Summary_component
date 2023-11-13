2bf8eecdfc59b57ceae7829103e4e11918bcc28b
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-11-13 18:28:38 -0500

    add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="border">
+      <p className="font-bold text-red-500">Work in progress...</p>
+    </div>
+  );
+}
+
+export default App;
