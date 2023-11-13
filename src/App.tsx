530b25e15515de0a8f99ea9873b58187513799cb
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-11-13 18:59:35 -0500

    add page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
-import "./App.css";
+import Card from "./components/Card";
+
+const DATA = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: "./assets/images/icon-reaction.svg",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: "./assets/images/icon-memory.svg",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: "./assets/images/icon-verbal.svg",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: "./assets/images/icon-visual.svg",
+  },
+];
 
 function App() {
   return (
-    <div className="border">
-      <p className="font-bold text-red-500">Work in progress...</p>
+    <div className="h-screen w-screen flex flex-col items-center justify-between">
+      <div className="h-2/5 border flex flex-col items-center justify-around w-full rounded-b-2xl bg-primary">
+        <p>Your results</p>
+        <div>
+          <p>76</p>
+          <p>of 100</p>
+        </div>
+        <p>Great</p>
+        <p>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </div>
+      <div className="h-3/5 flex flex-col justify-around">
+        <p>Summary</p>
+        <div>
+          {DATA.map((item) => {
+            return <Card />;
+          })}
+        </div>
+        <button>Continue</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+const Card = ({ title, icon, number, color }) => {
+  return <p>Card</p>;
+};
+
+export default Card;
