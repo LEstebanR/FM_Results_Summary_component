42b7084c698e083dd930c30eb9995be92b2ed07c
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-11-14 18:57:25 -0500

    add evaluation card

--- src/App.tsx
+++ src/App.tsx
@@ -25,15 +25,15 @@ const DATA = [
 
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-between">
-      <div className="h-2/5 border flex flex-col items-center justify-around w-full rounded-b-2xl bg-primary">
-        <p>Your results</p>
-        <div>
-          <p>76</p>
-          <p>of 100</p>
+    <div className="h-screen w-screen flex flex-col items-center justify-between md:justify-center md:flex-row">
+      <div className="h-2/5 border flex flex-col items-center justify-around  w-full md:w-1/4 rounded-b-2xl  md:rounded-2xl bg-gradient-to-b from-slateBlue to-royalBlue py-4">
+        <p className="text-lightLavender text-lg font-bold">Your Result</p>
+        <div className="p-2 rounded-full flex flex-col justify-center items-center bg-gradient-to-b from-violetBlue to-persianBlue h-24 w-24">
+          <p className="text-white text-4xl font-bold">76</p>
+          <p className="text-lightLavender text-sm">of 100</p>
         </div>
-        <p>Great</p>
-        <p>
+        <p className="text-white text-2xl">Great</p>
+        <p className="text-lightLavender text-sm w-2/3 text-center">
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     extend: {
       colors: {
         primary: "#646cff",
+        lightLavender: "hsl(241, 100%, 89%)",
+        slateBlue: "hsl(252, 100%, 67%)",
+        royalBlue: "hsl(241, 81%, 54%)",
+        violetBlue: "hsla(256, 72%, 46%, 1)",
+        persianBlue: "hsla(241, 72%, 46%, 0)",
+      },
+      fontFamily: {
+        sans: ["Hanken Grotesk"],
       },
     },
   },
