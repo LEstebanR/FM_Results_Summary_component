2981beb53899f4662a555c77a8a88064b08a0dad
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-11-16 21:13:07 -0500

    complete challenge

--- src/App.tsx
+++ src/App.tsx
@@ -38,8 +38,8 @@ const DATA = [
 function App() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-between md:justify-center md:flex-row ">
-      <div className="flex flex-col gap-1 h-full items-center">
-        <div className="h-2/5 border flex flex-col items-center justify-around  w-full md:w-1/2 rounded-b-2xl  md:rounded-2xl bg-gradient-to-b from-slateBlue to-royalBlue py-4 ">
+      <div className="w-full md:w-1/2 flex flex-col md:flex-row h-full md:h-1/2 items-center md:rounded-xl md:shadow-2xl">
+        <div className="h-2/5 md:h-full border flex flex-col items-center justify-around  w-full md:w-1/2 rounded-b-2xl  md:rounded-2xl bg-gradient-to-b from-slateBlue to-royalBlue py-4 ">
           <p className="text-lightLavender text-lg font-bold">Your Result</p>
           <div className="p-2 rounded-full flex flex-col justify-center items-center bg-gradient-to-b from-violetBlue to-persianBlue h-24 w-24">
             <p className="text-white text-4xl font-bold">76</p>
@@ -50,7 +50,7 @@ function App() {
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </div>
-        <div className="h-3/5 flex flex-col gap-1 justify-around items-start md:items-center w-11/12 md:w-1/2 md:h-2/5 pl-8  md:rounded-r-xl md:px-2 md:shadow-2xl">
+        <div className="h-3/5 flex flex-col gap-2 justify-around items-start md:items-center w-11/12 md:w-1/2 md:h-2/5 pl-8 md:px-4  ">
           <p className="text-lg font-bold">Summary</p>
           <div className="w-full flex flex-col items-center pr-8 md:pr-0 gap-2">
             {DATA.map((item) => {
@@ -65,7 +65,7 @@ function App() {
               );
             })}
           </div>
-          <button className=" px-4 bg-darkGrayBlue text-white py-3 rounded-full w-11/12 md:w-full hover:bg-red-500">
+          <button className=" px-4 bg-darkGrayBlue text-white py-3 rounded-full w-11/12 md:w-full hover:bg-gradient-to-b md:from-slateBlue md:to-royalBlue mt-4">
             Continue
           </button>
         </div>
